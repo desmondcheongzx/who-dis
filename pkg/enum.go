fd27d1853c775ebdc3b44820515335476666508b
desmondcheongzx <desmondcheongzx@users.noreply.github.com>
2022-05-06 21:50:30 +0000

    Apply automatic gofmt fixes

--- pkg/enum.go
+++ pkg/enum.go
@@ -1,6 +1,7 @@
 package pkg
 
 type RRType = uint16
+
 const (
 	RR_A     = 1
 	RR_NS    = 2
@@ -21,6 +22,7 @@ const (
 )
 
 type RRClass = uint16
+
 const (
 	RR_IN = 1
 	RR_CS = 2
@@ -29,6 +31,7 @@ const (
 )
 
 type QType = uint16
+
 const (
 	Q_A     = 1
 	Q_NS    = 2
@@ -46,17 +49,18 @@ const (
 	Q_MINFO = 14
 	Q_MX    = 15
 	Q_TXT   = 16
-	Q_AXFR = 252 
+	Q_AXFR  = 252
 	Q_MAILB = 253
 	Q_MAILA = 254
 	Q_TSTAR = 255
 )
 
 type QClass = uint16
+
 const (
-	Q_IN = 1
-	Q_CS = 2
-	Q_CH = 3
-	Q_HS = 4
+	Q_IN    = 1
+	Q_CS    = 2
+	Q_CH    = 3
+	Q_HS    = 4
 	Q_CSTAR = 255
 )
