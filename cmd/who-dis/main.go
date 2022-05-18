cf57a1f0db487998d696c83c4574c3b16343788b
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2022-05-18 17:52:18 -0400

    Add commandline flags
    Support options for recursive querying and caching.

--- cmd/who-dis/main.go
+++ cmd/who-dis/main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 
 	dns "github.com/desmondcheongzx/who-dis/pkg"
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	var nocacheFlag bool
+	flag.BoolVar(&nocacheFlag, "nocache", false, "Turn off caching.")
+	var recursiveFlag bool
+	flag.BoolVar(&recursiveFlag, "trace", false, "Turn on recursive queries.")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
 		fmt.Println("usage: ./who-dis <domain name>")
 		return
 	}
-	dn := os.Args[1]
+	dn := args[0]
 	client := dns.NewDNSClient()
-	err := client.Query(dn, true, true)
+	fmt.Printf("nochache = %v; trace = %v\n", nocacheFlag, recursiveFlag)
+	err := client.Query(dn, recursiveFlag, !nocacheFlag)
 	if err != nil {
 		fmt.Println(err)
 	}
